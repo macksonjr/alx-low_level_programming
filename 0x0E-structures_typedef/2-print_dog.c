bc7612447e60ce346f1d82c0f05079fb3b571797
macksonjr <mkharimasocha611@gmail.com>
2023-07-17 07:58:51 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog"
+#include "dog.h"
 
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
