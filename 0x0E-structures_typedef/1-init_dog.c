a287204b43c2ee8b847846d7c754c3a739107513
macksonjr <mkharimasocha611@gmail.com>
2023-07-17 06:39:01 -0700

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,11 @@
 
 /**
  * main - check the code
- *
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  * Return: Always 0.
  */
 
