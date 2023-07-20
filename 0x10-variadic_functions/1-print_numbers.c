322233ed167deca38f814e20b84491d43defc46f
macksonjr <mkharimasocha611@gmail.com>
2023-07-20 06:52:36 -0700

    sum

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,6 +3,10 @@
 #include <stdarg.h>
 
 /**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  *print a new line
  */
 void print_numbers(const char *separator, const unsigned int n, ...);
