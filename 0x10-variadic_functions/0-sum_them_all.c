9d458dd1898577d25b40fa819665bdef713fcaf3
macksonjr <mkharimasocha611@gmail.com>
2023-07-20 06:31:22 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *print sum of all parameters
+ *Return: 0
+*/
+
+int sum_them_all(const unsigned int n, ...);
+ {
+	 if(n == 0 )
+	 return 0;
+ }
+int main (void)
+{
+	int n;
+	n = sum of parameters;
+	printf("%d /n",n);
+	return sum;
+}
+
