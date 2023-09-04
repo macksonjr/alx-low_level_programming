2ae47f62959bb428438b04d897e7d2fbff820c40
macksonjr <mkharimasocha611@.com>
2023-09-04 11:41:50 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,3 +26,21 @@ char *create_buffer(char *file)
 
 	return (buffer);
 }
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
