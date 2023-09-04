75bb72e01bb8e5afa8d9ca884addbedf0e31bfd6
macksonjr <mkharimasocha611@.com>
2023-09-04 11:38:42 -0700

    update

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes.
+ * @file: The name of the file buffer.
+ *
+ * Return: A pointer to the newly-allocated.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
